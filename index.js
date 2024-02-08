420641a1c6a25ad66363d69cdc786d2d04ee9b4f
gsd <386083520@qq.com>
2024-02-08 08:51:56 +0800

    编写el-card基础结构

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 console.log("my-element-ui2")
 import EllButton from "./Button.vue"
+import EllCard from "./packages/card/main.vue"
 
 function install(Vue, opts = {}) {
     Vue.component("EllButton", EllButton)
+    Vue.component("EllCard", EllCard)
 }
 
 export default {
---
+++ packages/card/main.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="ell-card">
+        <div class="ell-card__header">
+            header
+        </div>
+        <div class="ell-card__body">
+            body
+        </div>
+    </div>
+</template>
