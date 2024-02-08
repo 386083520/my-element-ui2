fdd680ee283c721a23a81b958c7c63448c912d1f
gsd <386083520@qq.com>
2024-02-08 09:02:00 +0800

    编写el-card基础样式

--- packages/card/main.vue
+++ packages/card/main.vue
@@ -8,3 +8,15 @@
         </div>
     </div>
 </template>
+<style>
+    .ell-card {
+        border: 1px solid #EBEEF5;
+    }
+    .ell-card__header {
+        padding: 18px 20px;
+        border-bottom: 1px solid #EBEEF5;
+    }
+    .ell-card__body {
+        padding: 20px;
+    }
+</style>
