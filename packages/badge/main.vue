3ac9cf2bba8c16a6c64eeb6fc4a0f5e074d99c1f
gsd <386083520@qq.com>
2024-02-22 10:06:02 +0800

    编写el-badge基础样式

--- packages/badge/main.vue
+++ packages/badge/main.vue
@@ -1,8 +1,31 @@
 <template>
-    <div>
+    <div class="ell-badge">
         <slot></slot>
-        <sup>
+        <sup class="ell-badge__content is-fixed">
             8
         </sup>
     </div>
 </template>
+<style>
+    .ell-badge {
+        position: relative;
+        display: inline-block;
+    }
+    .ell-badge__content {
+        background-color: #F56C6C;
+        padding: 0 6px;
+        height: 18px;
+        line-height: 18px;
+        display: inline-block;
+        color: #fff;
+        font-size: 12px;
+        border-radius: 10px;
+        border: 1px solid;
+    }
+    .ell-badge__content.is-fixed {
+        position: absolute;
+        top: 0px;
+        right: 10px;
+        transform: translateY(-50%) translateX(100%);
+    }
+</style>
