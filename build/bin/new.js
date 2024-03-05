9e6f5e7bde8c1dce832d05126d445cad0e8bb56a
gsd <386083520@qq.com>
2024-03-05 13:37:37 +0800

    文件指定路径保存

--- build/bin/new.js
+++ build/bin/new.js
@@ -1,2 +1,6 @@
 const componentName = process.argv[2]
 console.log(componentName)
+const fileSave = require('file-save')
+const path = require('path')
+const PackagePath = path.resolve(__dirname, '../../packages', componentName)
+fileSave(path.join(PackagePath, "aaa.json")).write("abcd", 'utf8')
