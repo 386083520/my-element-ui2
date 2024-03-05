bc05cc786d4dedca05b0d15f6f476162b45c5a94
gsd <386083520@qq.com>
2024-03-05 17:37:54 +0800

    处理报错

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log("my-element-ui2")
-import EllButton from "./Button.vue"
-import EllCard from "./packages/card/index.js"
-import EllBadge from "./packages/badge/index.js"
+import EllButton from "../Button.vue"
+import EllCard from "../packages/card/index.js"
+import EllBadge from "../packages/badge/index.js"
 
 function install(Vue, opts = {}) {
     Vue.component("EllButton", EllButton)
