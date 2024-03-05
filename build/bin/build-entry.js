071df839306577614e007a5387f20ac88a61d018
gsd <386083520@qq.com>
2024-03-05 17:58:08 +0800

    render函数渲染index.js

--- build/bin/build-entry.js
+++ build/bin/build-entry.js
@@ -2,11 +2,32 @@ var render = require('json-templater/string')
 var fs = require('fs')
 var path = require('path')
 var MAIN_TEMPLATE = `
-import EllButton from "./Button.vue"
-import EllCard from "./packages/card/index.js"
-import EllBadge from "./packages/badge/index.js"`
-var OUTPUT_PATH = path.join(__dirname, '../../src/index.js')
-var template = render(MAIN_TEMPLATE, {
+{{include}}
 
+const components = [
+    {{install}}
+]
+function install(Vue, opts = {}) {
+   components.forEach(component => {
+    Vue.component(component.name, component)
+   })
+}
+
+export default {
+    install
+}`
+var OUTPUT_PATH = path.join(__dirname, '../../src/index.js')
+var includeComponentTemplate = [
+    'import EllButton from "../Button.vue"',
+    'import EllCard from "../packages/card/index.js"',
+    'import EllBadge from "../packages/badge/index.js"'
+]
+var installTemplate = [
+    'EllCard',
+    'EllBadge'
+]
+var template = render(MAIN_TEMPLATE, {
+    include: includeComponentTemplate.join('\n'),
+    install: installTemplate.join(',\n')
 })
 fs.writeFileSync(OUTPUT_PATH, template)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-console.log("my-element-ui2")
+
 import EllButton from "../Button.vue"
 import EllCard from "../packages/card/index.js"
 import EllBadge from "../packages/badge/index.js"
 
+const components = [
+    EllCard,
+EllBadge
+]
 function install(Vue, opts = {}) {
-    Vue.component("EllButton", EllButton)
-    Vue.component("EllCard", EllCard)
-    Vue.component("EllBadge", EllBadge)
+   components.forEach(component => {
+    Vue.component(component.name, component)
+   })
 }
 
 export default {
