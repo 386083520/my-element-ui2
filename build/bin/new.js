b1f1a11c5430f1c7af2d267595d57b8b42941bac
gsd <386083520@qq.com>
2024-03-05 13:48:02 +0800

    驼峰命名转换；代码生成组件的index文件

--- build/bin/new.js
+++ build/bin/new.js
@@ -1,6 +1,21 @@
-const componentName = process.argv[2]
-console.log(componentName)
+const componentname = process.argv[2]
+console.log(componentname)
 const fileSave = require('file-save')
 const path = require('path')
-const PackagePath = path.resolve(__dirname, '../../packages', componentName)
-fileSave(path.join(PackagePath, "aaa.json")).write("abcd", 'utf8')
+const uppercamelcase = require('uppercamelcase')
+const PackagePath = path.resolve(__dirname, '../../packages', componentname)
+const ComponentName = uppercamelcase(componentname)
+
+const fileInfo =  {
+    filename: 'index.js',
+    content: `
+    import ${ComponentName} from './src/main.vue'
+    ${ComponentName}.install = function(Vue) {
+        Vue.component(${ComponentName}.name, ${ComponentName})
+    }
+    export default ${ComponentName}
+    `
+}
+
+
+fileSave(path.join(PackagePath, fileInfo.filename)).write(fileInfo.content, 'utf8')
