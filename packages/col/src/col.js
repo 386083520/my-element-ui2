95b3c0f05494175d1548154e97c3498a62f4b323
gsd <386083520@qq.com>
2024-04-17 08:57:51 +0800

    响应式布局实现

--- packages/col/src/col.js
+++ packages/col/src/col.js
@@ -5,7 +5,12 @@ export default {
             type: Number,
             default: 24
         },
-        offset: Number
+        offset: Number,
+        xs: Number,
+        sm: Number,
+        md: Number,
+        lg: Number,
+        xl: Number
     },
     render(h) {
         let classList = []
@@ -17,6 +22,11 @@ export default {
         ['span', 'offset'].forEach(prop => {
             if(this[prop]) {
                 classList.push(prop !== 'span'? `ell-col-${prop}-${this[prop]}`:`ell-col-${this[prop]}`)
+            }
+        })
+        ['xs', 'sm', 'md', 'lg', 'xl'].forEach(size => {
+            if(typeof this[size] === 'number') {
+                classList.push(`ell-col-${size}-${this[size]}`)
             }
         })
         return h('div', {
