e1df561712ed54c95ad39fd5303eb4e098a34087
gsd <386083520@qq.com>
2024-07-05 17:31:29 +0800

    隐藏原生滚动条

--- packages/scrollbar/src/main.js
+++ packages/scrollbar/src/main.js
@@ -1,11 +1,14 @@
 export default {
     name: 'EllScrollbar',
     render(h) {
+        let gutter = 17
+        const gutterWidth = `-${gutter}px`
+        const gutterStyle = `margin-right:${gutterWidth};`
         const  view = h('div', {
             class: ['ell-scrollbar__view']
         }, this.$slots.default)
         let nodes = ([
-            <div class={['ell-scrollbar__wrap']}>
+            <div style={gutterStyle} class={['ell-scrollbar__wrap']}>
                 { [view] }
             </div>
         ])
