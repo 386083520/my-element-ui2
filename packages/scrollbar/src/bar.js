25b4fb140642dd0eaebd74439dadb80f7e09b295
gsd <386083520@qq.com>
2024-07-06 09:37:25 +0800

    bar点击事件

--- packages/scrollbar/src/bar.js
+++ packages/scrollbar/src/bar.js
@@ -6,13 +6,24 @@ export default {
         size: String,
         move: Number
     },
+    computed: {
+        wrap() {
+            return this.$parent.wrap
+        }
+    },
     render(h) {
         const { size, move } = this;
         return (
-            <div class={['ell-scrollbar__bar', 'is-vertical']}>
+            <div class={['ell-scrollbar__bar', 'is-vertical']} onMousedown={this.clickTrackHandler}>
                 <div class='ell-scrollbar__thumb' style={ renderThumbStyle({size, move}) }>
                 </div>
             </div>
         )
+    },
+    methods: {
+        clickTrackHandler(e) {
+            console.log(e)
+            this.wrap.scrollTop = 500
+        }
     }
 }
