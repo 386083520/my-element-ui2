2601af46ace008d163b6d0c280f48caa30139996
gsd <386083520@qq.com>
2024-07-20 17:23:10 +0800

    disabled属性

--- packages/input/src/main.vue
+++ packages/input/src/main.vue
@@ -1,10 +1,27 @@
 <template>
-    <div class="ell-input">
-      <input class="ell-input__inner" v-bind="$attrs"/>
+    <div :class="[
+        'ell-input',
+        {
+          'is-disabled': inputDisabled
+        }
+    ]">
+      <input
+          class="ell-input__inner"
+          v-bind="$attrs"
+          :disabled="inputDisabled"
+      />
     </div>
 </template>
 <script>
     export default {
-        name: 'EllInput'
+        name: 'EllInput',
+      props: {
+          disabled: Boolean
+      },
+      computed: {
+          inputDisabled() {
+            return this.disabled
+          }
+      }
     }
 </script>
