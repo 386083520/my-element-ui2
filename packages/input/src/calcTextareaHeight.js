e19ca3c4791a307d69934b4f63e8ef0d4957cb45
gsd <386083520@qq.com>
2024-07-21 17:01:25 +0800

    设置临时textarea样式

--- packages/input/src/calcTextareaHeight.js
+++ packages/input/src/calcTextareaHeight.js
@@ -8,11 +8,37 @@ z-index:-1000;
 top:0;
 right:0;
 `
+
+const CONTEXT_STYLE = [
+    'letter-spacing',
+    'line-height',
+    'padding-top',
+    'padding-bottom',
+    'font-family',
+    'font-weight',
+    'font-size',
+    'text-rendering',
+    'text-transform',
+    'width',
+    'text-indent',
+    'padding-left',
+    'padding-right',
+    'border-width',
+    'box-sizing'
+];
+
+function calculateNodeStyling(targetElement) {
+    const  style = window.getComputedStyle(targetElement)
+    const contextStyle = CONTEXT_STYLE.map(name => `${name}:${style.getPropertyValue(name)}`)
+    return { contextStyle }
+}
 export default function calcTextareaHeight(targetElement) {
     if(!hiddenTextarea) {
         hiddenTextarea = document.createElement('textarea')
         document.body.appendChild(hiddenTextarea)
     }
+    let { contextStyle } = calculateNodeStyling(targetElement)
+    hiddenTextarea.setAttribute('style', `${contextStyle}`)
     hiddenTextarea.value = targetElement.value || targetElement.placeholder || ''
     let height = hiddenTextarea.scrollHeight
     const result = {}
