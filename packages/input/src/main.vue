6cc30daedc7fa28a099ef36b657b7ed7c0f5b7b5
gsd <386083520@qq.com>
2024-07-21 10:29:55 +0800

    可清空逻辑实现

--- packages/input/src/main.vue
+++ packages/input/src/main.vue
@@ -6,13 +6,19 @@
         }
     ]">
       <input
+          ref="input"
           class="ell-input__inner"
           v-bind="$attrs"
           :disabled="inputDisabled"
+          @input="handleInput"
       />
       <span class="ell-input__suffix">
         <span class="ell-input__suffix-inner">
-          <i class="el-icon-circle-close ell-input__icon ell-input__clear"></i>
+          <i
+              v-if="showClear"
+              class="el-icon-circle-close ell-input__icon ell-input__clear"
+              @click="clear"
+          ></i>
         </span>
       </span>
     </div>
@@ -21,12 +27,41 @@
     export default {
         name: 'EllInput',
       props: {
-          disabled: Boolean
+          disabled: Boolean,
+          clearable: {
+              type: Boolean,
+              default: false
+          },
+          value: [String, Number]
       },
       computed: {
           inputDisabled() {
             return this.disabled
-          }
+          },
+        showClear() {
+            return this.clearable && this.nativeInputValue
+        },
+        nativeInputValue() {
+            return this.value === null || this.value === undefined ? '': String(this.value)
+        }
+      },
+      methods: {
+        handleInput(event) {
+          this.$emit('input', event.target.value)
+        },
+        clear() {
+          this.$emit('input', '')
+        },
+        setNativeInputValue() {
+          const input = this.$refs.input
+          if(!input) return
+          input.value = this.nativeInputValue
+        }
+      },
+      watch: {
+        nativeInputValue() {
+          this.setNativeInputValue()
+        }
       }
     }
 </script>
