32d183aba54085dda4ce446a05dce7d23aba3106
gsd <386083520@qq.com>
2024-08-05 12:10:51 +0800

    increase操作

--- packages/input-number/src/main.vue
+++ packages/input-number/src/main.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="ell-input-number">
-        <span class="ell-input-number__decrease">
+        <span
+            class="ell-input-number__decrease"
+            @click="descrease"
+        >
             <i class="el-icon-minus"></i>
         </span>
-        <span class="ell-input-number__increase">
+        <span
+            class="ell-input-number__increase"
+            @click="increase"
+        >
             <i class="el-icon-plus"></i>
         </span>
         <ell-input
-            :value="value"
+            :value="displayValue"
             @input="handleInput"
         />
     </div>
@@ -16,12 +22,36 @@
     export default {
         name: 'EllInputNumber',
         props: {
-            value: {}
+            value: {},
+        },
+        data() {
+            return {
+                currentValue: 0
+            }
         },
         methods: {
             handleInput(val) {
                 console.log(val)
                 this.$emit('input', val)
+            },
+            setCurrentValue(newVal) {
+                this.currentValue = newVal
+                this.$emit('input', newVal)
+            },
+            increase() {
+                const value = this.value || 0
+                this.setCurrentValue(value + 1)
+            },
+            descrease() {
+
+            }
+        },
+        computed: {
+            displayValue() {
+                if(this.value) {
+                    return this.value
+                }
+                return this.currentValue
             }
         }
     }
