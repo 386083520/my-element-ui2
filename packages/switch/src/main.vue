9875bcf6fb5fe78517ad8377eb4786ed644fda26
gsd <386083520@qq.com>
2024-11-07 15:27:29 +0800

    switch基础逻辑

--- packages/switch/src/main.vue
+++ packages/switch/src/main.vue
@@ -1,14 +1,45 @@
 <template>
-    <div class="ell-switch is-checked">
+    <div
+        class="ell-switch"
+        :class="{ 'is-checked': checked }"
+    >
         <input
             type="checkbox"
             class="ell-switch__input"
+            @change="handleChange"
+            :true-value="activeValue"
+            :false-value="inactiveValue"
         />
         <span class="ell-switch__core"></span>
     </div>
 </template>
 <script>
     export default {
-        name: 'EllSwitch'
+        name: 'EllSwitch',
+        props: {
+            value: {
+                type: [String, Number, Boolean],
+                default: false
+            },
+            activeValue: {
+                type: [String, Number, Boolean],
+                default: true
+            },
+            inactiveValue: {
+                type: [String, Number, Boolean],
+                default: false
+            }
+        },
+        computed: {
+            checked() {
+                return this.value === this.activeValue
+            }
+        },
+        methods: {
+            handleChange() {
+                const val = this.checked ? this.inactiveValue: this.activeValue
+                this.$emit('input', val)
+            }
+        }
     }
 </script>
