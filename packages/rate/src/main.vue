c942a2864698f643856bb696d28c38647b06ba5e
gsd <386083520@qq.com>
2024-11-15 15:03:34 +0800

    rate基础样式和结构

--- packages/rate/src/main.vue
+++ packages/rate/src/main.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="ell-rate">
-        <span v-for="(item,key) in 5" :key="key">
-            <i>{{item}}</i>
+        <span v-for="(item,key) in 5" :key="key" class="ell-rate__item">
+            <i
+                :class="classes[item - 1]"
+                class="ell-rate__icon"
+            >
+
+            </i>
         </span>
     </div>
 </template>
 <script>
     export default {
-        name: 'EllRate'
+        name: 'EllRate',
+        computed: {
+            classes() {
+                let result = []
+                for (let i = 0; i < 5; i++) {
+                    result.push('el-icon-star-off')
+                }
+                return result
+            }
+        }
     }
 </script>
