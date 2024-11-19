63156a5b65fd4e826e5133b1f209cb3a226cec6c
gsd <386083520@qq.com>
2024-11-19 15:52:52 +0800

    rate鼠标移动效果

--- packages/rate/src/main.vue
+++ packages/rate/src/main.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="ell-rate">
-        <span v-for="(item,key) in 5" :key="key" class="ell-rate__item">
+        <span
+            v-for="(item,key) in 5"
+            :key="key"
+            class="ell-rate__item"
+            :style="{ cursor:  'pointer' }"
+            @mousemove="setCurrentValue(item)"
+            @mouseleave="resetCurrentValue(item)"
+        >
             <i
-                :class="classes[item - 1]"
+                :class="[classes[item - 1], {'hover': hoverIndex === item}]"
                 class="ell-rate__icon"
+                :style="getIconStyle(item)"
             >
 
             </i>
@@ -13,13 +21,38 @@
 <script>
     export default {
         name: 'EllRate',
+        data() {
+            return {
+                currentValue: 0,
+                hoverIndex: 0
+            }
+        },
         computed: {
             classes() {
                 let result = []
-                for (let i = 0; i < 5; i++) {
+                let i = 0;
+                for (; i < this.currentValue; i++) {
+                    result.push('el-icon-star-on')
+                }
+                for (; i < 5; i++) {
                     result.push('el-icon-star-off')
                 }
                 return result
+            }
+        },
+        methods: {
+            setCurrentValue(value) {
+                this.currentValue = value
+                this.hoverIndex = value
+            },
+            resetCurrentValue() {
+                this.currentValue = 0
+                this.hoverIndex = 0
+            },
+            getIconStyle(item) {
+                return {
+                    color: item <= this.currentValue ? '#F7BA2A' : '#C6D1DE'
+                }
             }
         }
     }
